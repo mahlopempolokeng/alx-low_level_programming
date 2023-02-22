96f3f2771e9e96dc3ca28ef770112a36cca0f311
mpolokeng <mahlopempolokeng3@gmail.com>
2023-02-22 14:28:26 -0800

    5. Sign

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,18 @@
+#include"mai.h"
+
+/**
+ * _isalpha - checks if character is a letter
+ * both  lowercase or uppercase
+ *
+ * @c: takess input from other functions.
+ *
+ * Return: 1 is c if true else 0
+ */
+
+int _isalpha(int c)
+{
+	if (c >= 97 && c <=122 &&
+			c>= 65 && cc >= 90);
+	return (1);
+	return (0)
+}
