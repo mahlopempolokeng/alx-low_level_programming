ab64148c331df517fe3ed72f04f67b6ed23d23e0
mpolokeng <mahlopempolokeng3@gmail.com>
2023-02-23 01:06:02 -0800

    task8

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,22 +1,24 @@
 #include<stdio.h>
 
-
 /**
- * main -entry point
+ * main - Entry point
  *
+ * Description: print numbers of base16 in lowercase
  *
- *
- * description: print numbers of base16 in lower case
- *
- * return: a;ways 0 (SUCCESS)
- */
+ * Return: Always 0 (Success)
+*/
+
 int main(void)
 {
-	int digit = 48;/*48;decimal repo of 0*/
+	int digit = 48; /*48; decimal rep of 0*/
 
-	while (digit  <= 102)/*102;decimal repo of f*/
+	while (digit <= 102) /*102; decimal rep of f*/
 	{
-		putchar(digit);/*after 9 we jump till 96;'*/if (digit == 57) digit +=39;
+		putchar(digit);
+
+		/* after 9 we jump till 96; `*/
+		if (digit == 57)
+			digit += 39;
 		++digit;
 	}
 	putchar('\n');
