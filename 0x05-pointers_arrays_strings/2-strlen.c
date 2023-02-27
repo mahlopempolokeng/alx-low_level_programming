3524666ebd6cbf1dc27ddad5b558881eca112879
mpolokeng <mahlopempolokeng3@gmail.com>
2023-02-27 07:21:39 -0800

    task2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+/**
+/**
+ * _strlen - find lenght of a string
+ * @s: pointer to the string to check
+ * Return: void
+ *
+ */
+int _strlen(char *s)
+{
+	int i = o;
+
+	while ([i])
+		i++;
+	return (i);
+}
