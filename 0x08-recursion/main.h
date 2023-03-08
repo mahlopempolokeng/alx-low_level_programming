18175ede67236d5fcf97454e3b9c89c5833b406f
mpolokeng <mahlopempolokeng3@gmail.com>
2023-03-08 04:41:15 -0800

    task0

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -1,6 +1,19 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+/* This a header file containing declarations
+ * for prototype functions defined in the 0x08-recursion
+ * tasks and directory
+ * This is like creating my own library with declared functions
+ * The tasks defines  the functions
+ *
+ *
+ * enclosing the files  contentinbetween ifndef,define and endif,
+ * stops the compiler from processing the file twice if its included twice,
+ * if not so, this will cause an error
+ * */
+
+int _putchar(char c);
 void _puts_recursion(char *s);
 void _print_rev_recursion(char *s);
 int _strlen_recursion(char *s);
@@ -12,3 +25,4 @@ int is_palindrome(char *s);
 int wildcmp(char *s1, char *s2);
 
 #endif
+
