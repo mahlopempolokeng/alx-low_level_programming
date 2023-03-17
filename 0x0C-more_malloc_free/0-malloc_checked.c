4ad895a82a04a45b111dddef66e22332ba956208
mpolokeng <mahlopempolokeng3@gmail.com>
2023-03-17 05:48:41 -0700

    TASK0

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,19 +1,19 @@
 #include <stdlib.h>
-
-
+#include "main.h"
 /**
- * *malloc_checked - allocates memory using malloc and exit if failed
- * @b: int
- * Return: pointer to the array initialized or NULL
+ * malloc_checked - allocates memory using malloc
+ * @b: number of bytes in memory to allocate
+ * Return: pointer to allocated memory(Success), 98 on failure
  */
-
 void *malloc_checked(unsigned int b)
 {
-	int *m = malloc(b);
+	void *ptr;
 
-	if (m == 0)
+	ptr = malloc(b);
+
+	if (ptr == NULL)
 		exit(98);
 
-	return (m);
+	return (ptr);
 }
 
--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -1,9 +1,18 @@
-#ifndef HEADER_FILE
-#define HEADER_FILE
-int *array_range(int min, int max);
-void *_calloc(unsigned int nmemb, unsigned int size);
-char *string_nconcat(char *s1, char *s2, unsigned int n);
+#ifndef MAIN_H
+#define MAIN_H
+/**
+ * This is a header file that contains function
+ * prototypes that will be used in this project
+ */
 int _putchar(char c);
 void *malloc_checked(unsigned int b);
+char *string_nconcat(char *s1, char *s2, unsigned int n);
+void *_calloc(unsigned int nmemb, unsigned int size);
+int *array_range(int min, int max);
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
+void errors(void);
+int _strlen(char *s);
+int is_digit(char *s);
 
-#endif
+#endif /**end of main.h file*/
+
